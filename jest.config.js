b7bae7db56b055945a5a4f279ccb568d24ac6360
avnerjose <avnerj.g.r@gmail.com>
2022-09-05 20:28:32 -0300

    Ignoring cypress on jest

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,17 @@
 module.exports = {
-	testPathIgnorePatterns: ["/node_modules", "/.next"],
-	setupFilesAfterEnv: [
-		"<rootDir>/src/setupTests.ts"
-	],
-	transform: {
-		"^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest"
-	},
-	testEnvironment: "jsdom",
-	collectCoverage: true,
-	collectCoverageFrom: [
-		"src/**/*.(tsx)",
-		"!src/**/*.test.(tsx)",
-		"!src/**/_app.tsx",
-		"!src/**/_document.tsx",
-		"!src/**/components/CategoryIcon.tsx",
-
-	],
-	coverageReporters: ["lcov", "json"]
-}
+  testPathIgnorePatterns: ["/node_modules", "/.next", "/cypress"],
+  setupFilesAfterEnv: ["<rootDir>/src/setupTests.ts"],
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
+  },
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.(tsx)",
+    "!src/**/*.test.(tsx)",
+    "!src/**/_app.tsx",
+    "!src/**/_document.tsx",
+    "!src/**/components/CategoryIcon.tsx",
+  ],
+  coverageReporters: ["lcov", "json"],
+};
